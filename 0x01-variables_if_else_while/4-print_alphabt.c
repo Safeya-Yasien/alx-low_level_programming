b1634641ed2f9076b887fdbb8e17c7e9836d0fda
safeya yasien <110609863+Safeya-Yasien@users.noreply.github.com>
2023-02-17 14:41:32 +0200

    Create 4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ *main - lower alphabet and upper except q and e
+ *
+ *print alphabet by putchar function which print without press any tab
+ *
+ *Return: zero of success
+ */
+int main(void)
+{
+char a;
+for (a = 'a'; a <= 'z'; a++)
+{
+if(a == 'q' || a == 'e')
+continue;
+putchar(a);
+}
+putchar('\n');
+return (0);
+}
