8a367b139f4fbd882b51d1a9dd5125fc9721a14c
safeya yasien <110609863+Safeya-Yasien@users.noreply.github.com>
2023-02-17 14:36:31 +0200

    Create 3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ *main - lower alphabet and upper
+ *
+ *print alphabet by putchar function which print without press any tab
+ *
+ *Return: zero of success
+ */
+int main(void)
+{
+char a;
+for (a = 'a'; a <= 'z'; a++)
+{
+putchar(a);
+}
+putchar('\n'); 
+for (a = 'A'; a <= 'Z'; a++)
+{
+putchar(a);
+}
+putchar('\n');
+return (0);
+}
