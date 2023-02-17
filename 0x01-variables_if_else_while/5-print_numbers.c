06c5c255ff2a4edd28b44a9e26d3bc63f725aa2d
safeya yasien <110609863+Safeya-Yasien@users.noreply.github.com>
2023-02-17 14:44:39 +0200

    Create 5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ *main - lower alphabet and upper except q and e
+ *
+ *print alphabet by putchar function which print without press any tab
+ *
+ *Return: zero of success
+ */
+int main(void)
+{
+int i;
+for (i = 0; i <= 9; i++)
+printf("%d", i);
+printf("\n);
+return (0);
+}
