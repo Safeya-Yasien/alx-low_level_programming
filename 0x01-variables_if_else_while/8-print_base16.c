26dc4415ea5b583b6450d80cf88b41d7f52cbe4f
safeya yasien <110609863+Safeya-Yasien@users.noreply.github.com>
2023-02-18 00:51:00 +0200

    Create 8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ *main - lower alphabet base 16
+ *
+ *print alphabet by putchar function which print without press any tab
+ *
+ *Return: zero of success
+ */
+int main(void)
+{
+char a;
+for (a = '48'; a >= '57'; a++)
+{
+putchar(a);
+}
+for (a = '97'; a >= '102'; a++)
+{
+putchar(a);
+}  
+putchar('\n');
+return (0);
+}
