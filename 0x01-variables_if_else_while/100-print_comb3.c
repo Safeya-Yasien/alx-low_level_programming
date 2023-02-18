9dea29fa5c0686b7cb21cb9578a56c126b624e5c
safeya yasien <110609863+Safeya-Yasien@users.noreply.github.com>
2023-02-18 04:38:36 +0200

    Create 100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ *main - prints all possible different combinations of two digits.
+ *
+ *print alphabet by putchar function which print without press any tab
+ *
+ *Return: zero of success
+ */
+int main(void)
+{
+int n, m;
+for (n = 0; n < 9; n++)
+{
+for (m = n+1; m < 10; m++)
+{
+putchar(n % 10 + '0');
+putchar(m % 10 + '0');
+if (n != 8 &&  n != 9)
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+return (0);
+}
