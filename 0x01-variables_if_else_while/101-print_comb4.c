e4a8b551453efe9f98f4bb8031b9c14a8d43e83a
safeya yasien <110609863+Safeya-Yasien@users.noreply.github.com>
2023-02-18 06:25:32 +0200

    Create 101-print_comb4.c

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+/**
+ *main - prints all possible different combinations of three digits.
+ *
+ *print alphabet by putchar function which print without press any tab
+ *
+ *Return: zero of success
+ */
+int main(void)
+{
+int n, m, l;
+for (n = 0; n < 9; n++)
+{
+for (m = n+1; m < 10; m++)
+{
+for (l = m + 1; l < 10; l++)
+{
+putchar(n % 10 + '0');
+putchar(m % 10 + '0');
+putchar(l % 10 + '0');
+if (n != 8 &&  n != 9)
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+}
+putchar('\n');
+return (0);
+}
