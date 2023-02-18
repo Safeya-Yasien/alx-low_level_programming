41888d0c9f8bb68b94be6beeaca9624e4664cb2e
safeya yasien <110609863+Safeya-Yasien@users.noreply.github.com>
2023-02-18 06:27:48 +0200

    Update 101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -11,14 +11,14 @@ int main(void)
 int n, m, l;
 for (n = 0; n < 9; n++)
 {
-for (m = n+1; m < 10; m++)
+for (m = n + 1; m < 10; m++)
 {
 for (l = m + 1; l < 10; l++)
 {
 putchar(n % 10 + '0');
 putchar(m % 10 + '0');
 putchar(l % 10 + '0');
-if (n != 8 &&  n != 9)
+if (n != 8 &&  n != 9 && n != 7)
 {
 putchar(',');
 putchar(' ');
