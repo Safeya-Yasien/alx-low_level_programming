f1cbb90e32327dadafb5a212699325d0e4f748f1
safeya yasien <110609863+Safeya-Yasien@users.noreply.github.com>
2023-02-21 15:10:05 +0200

    Create 1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+char a;
+for(a = 'a'; a <= 'z'; a++)
+{
+putchar(a);
+}
+putchar('\n');
+return (0);
+}
