22cbe72bc67c897c60b99278b5391913fc9e86e9
safeya yasien <110609863+Safeya-Yasien@users.noreply.github.com>
2023-02-22 02:10:19 +0200

    Update 5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -10,17 +10,17 @@ int print_sign(int n)
 {
 if (n > 0)
 {
-putchar('+1');
+_putchar('+');
 return (1);
 }
 else if (n == 0)
 {
-putchar('00');
+_putchar('0');
 return (0);
 }
 else
 {
-putchar('-1');
+_putchar('-');
 return (-1);
 }
 }
