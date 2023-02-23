6ea39dae2b511066935ef1ffd78c24148a5020c2
safeya yasien <110609863+Safeya-Yasien@users.noreply.github.com>
2023-02-23 09:17:07 +0200

    Create 5-more_numbers.c

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+  * main - Write a function that prints 10 times the numbers, from 0 to 14, followed by a new line.
+  *
+  * Return: zero on success
+  */
+
+void more_numbers(void)
+{
+int i, j;
+for (i = 0; i <= 10; i++)
+{
+for (j = 0; j <= 14; j++)
+{
+if (j >= 10)
+{
+putchar(j / 10 + '0');
+}
+putchar(j % 10 + '0');
+}
+putchar('\n');
+}
+return;
+}
