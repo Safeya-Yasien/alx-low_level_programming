29304c61536c45d2ebc46ede179e32a986ec36c7
safeya yasien <110609863+Safeya-Yasien@users.noreply.github.com>
2023-02-23 07:58:21 +0200

    Create 102-fibonacci.c

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+  * main - print the first 50 fibonacci numbers.
+  * Return: Nothing.
+  */
+ 
+int main(void)
+{
+int n1 = 0, n2 = 1, next, i;
+printf("%d, %d, ", n1, n2);
+next = n1 + n2;
+for (i = 3; i <= 50; i++)
+{
+printf("%d, ", next);
+n1 = n2;
+n2 = next;
+next = n1 + n2;
+}
+printf("\n");
+return (0);
+}
