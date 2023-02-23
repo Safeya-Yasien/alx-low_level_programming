e3225251f354f62b9557255f403e96c5eedcb784
safeya yasien <110609863+Safeya-Yasien@users.noreply.github.com>
2023-02-23 13:47:33 +0200

    Update 9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -2,13 +2,12 @@
 
 /**
  * main - Prints the numbers 1 - 100,
- *   instead of multiples of 3, print Fizz
- *   instead of multiples of 5, print Buzz
- *   for multiples of 3 and 5, print FizzBuzz.
+ * instead of multiples of 3, print Fizz
+ * instead of multiples of 5, print Buzz
+ * for multiples of 3 and 5, print FizzBuzz.
  *
  * Return: 0 on success.
  */
-
 int main(void)
 {
 int i;
