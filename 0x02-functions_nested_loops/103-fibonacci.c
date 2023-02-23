dd873ff282665fd2b27798f96594ab6be3976e24
safeya yasien <110609863+Safeya-Yasien@users.noreply.github.com>
2023-02-23 14:34:51 +0200

    Update 103-fibonacci.c

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -9,12 +9,16 @@ int main(void)
 {
 unsigned long n1 = 0, n2 = 1, next, sum;
 next = n1 + n2;
-while(1)
+while (1)
 {
 if (next > 4000000)
-    break;
+{
+break;
+}
 if (next % 2 == 0)
-    sum += next;
+{
+sum += next;
+}
 n1 = n2;
 n2 = next;
 next = n1 + n2;
