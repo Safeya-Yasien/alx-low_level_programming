987a66524550ebb1424df13db0baf59b62c4500d
safeya yasien <110609863+Safeya-Yasien@users.noreply.github.com>
2023-02-23 08:01:55 +0200

    Update 102-fibonacci.c

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -7,11 +7,11 @@
  
 int main(void)
 {
-int n1 = 0, n2 = 1, next, i;
+long long int n1 = 0, n2 = 1, next, i;
 next = n1 + n2;
 for (i = 3; i <= 50; i++)
 {
-printf("%d, ", next);
+printf("%ld, ", next);
 n1 = n2;
 n2 = next;
 next = n1 + n2;
