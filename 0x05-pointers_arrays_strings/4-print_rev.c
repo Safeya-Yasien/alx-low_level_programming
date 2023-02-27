6619c447995a2850e204c37dc7a2e913a079318a
safeya yasien <110609863+Safeya-Yasien@users.noreply.github.com>
2023-02-27 19:28:09 +0200

    Update 4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -9,12 +9,15 @@
  */
 void print_rev(char *s)
 {
-int length = strlen(s);
-for (int i = 0; i < length / 2; i++)
+int length = 0;
+while (*s != '\0')
 {
-int temp = s[i];
-s[i] = s[length -i - 1];
-s[length -i - 1] = temp;
+length++;
 }
+for (int i = length - 1; i >= 0; i--)
+{
+putchar(s[i]);
+}
+putchar('\n');
 return;
 }
