4bef5bb148d48718e1628dedc80038c27f35c13a
safeya yasien <110609863+Safeya-Yasien@users.noreply.github.com>
2023-02-28 01:29:54 +0200

    Update 4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -7,13 +7,13 @@
 void print_rev(char *s)
 {
 int index = 0, len = 0;
-while (str[index++])
+while (s[index++])
 {
 len++;
 }
 for (index = 0; index < len; index += 2)
 {
-_putchar(str[index]);
+_putchar(s[index]);
 }
 _putchar('\n');
 }
