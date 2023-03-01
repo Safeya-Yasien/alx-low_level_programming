1631512225447e14f86ee61946443eb42d177729
Safeya-Yasien <bluesky7yas@gmail.com>
2023-03-01 22:38:12 +0200

    copy string

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * _strcat - function that concatenates two strings.
+ * @dest the destination string
+ * @src the source string
+ * @n number of bytes you want to copy
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+    int length = 0, index = 0, i;
+
+    while (dest[index++])
+    {
+        length++;
+    }
+
+    for (i = 0; src[i] && i < n; i++)
+    {
+        dest[i] = src[i];
+    }
+
+    for (i = length; i < n; i++)
+    {
+        dest[i] = '\0';
+    }
+
+    return (dest);
+}
