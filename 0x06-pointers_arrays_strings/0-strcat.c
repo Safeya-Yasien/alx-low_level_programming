099ea03e62110e94bfe9cef51c2afbc848c659ad
Safeya-Yasien <bluesky7yas@gmail.com>
2023-03-01 21:48:27 +0200

    concatenates two strings

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -14,7 +14,7 @@ char *_strcat(char *dest, char *src)
         length++;
     }
 
-    for (i = 0; dest[i] != '\0'; i++)
+    for (i = 0; src[i] != '\0'; i++)
     {
         dest[i + length] = src[i];
     }
