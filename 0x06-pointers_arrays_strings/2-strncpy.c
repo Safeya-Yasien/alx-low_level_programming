cd2dea7fd68b2be7ef8865e1da87c879291cf2ad
safeya yasien <110609863+Safeya-Yasien@users.noreply.github.com>
2023-03-02 08:32:52 +0200

    Update 2-strncpy.c

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -12,18 +12,21 @@
  */
 char *_strncpy(char *dest, char *src, int n)
 {
-int index = 0, src_len = 0;
-while (src[index++])
+int i, j;
+char sep[] = {' ', '\t', '\n', ',', ';', '.', '!', '?', '"', '(', ')', '{', '}'};
+for (i = 0; str[i]; i++)
 {
-src_len++;
-}
-for (index = 0; src[index] && index < n; index++)
+if (str[i] >= 'a' && str[i] <= 'z')
 {
-dest[index] = src[index];
+str[i] = str[i] - 32;
 }
-for (index = src_len; index < n; index++)
+for (j = 0; sep[j] != '\0'; j++)
 {
-dest[index] = '\0';
+if (sep[j] == str[i])
+{
+break;
 }
-return (dest);
+}
+}
+return (str);
 }
