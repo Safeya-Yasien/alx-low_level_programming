28caa6caa85e85a310899434099a56a88c39eee4
safeya yasien <110609863+Safeya-Yasien@users.noreply.github.com>
2023-03-06 12:48:50 +0200

    Update 0-memset.c

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -3,7 +3,7 @@
 /**
  * _memset - function that fills memory with a constant byte.
  * @s the string which we will replace its chars
- * @b the char which we will put it 
+ * @b the char which we will put it
  * @n the number of char we will replace
  * Return: pointer to the memory area s
  */
