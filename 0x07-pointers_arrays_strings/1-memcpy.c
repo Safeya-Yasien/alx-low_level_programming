a53c0e62d6bba3aa06c0cf94b701d783865a7175
Safeya-Yasien <bluesky7yas@gmail.com>
2023-03-06 13:20:04 +0200

    memcpy

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -3,8 +3,8 @@
 /**
  * _memcpy - function that copies memory area.
  * @dest: pointer to desitnation array
- * @ src: pointer to the sorce array
- * @ n: number of bytes to be copied from source to destination
+ * @src: pointer to the sorce array
+ * @n: number of bytes to be copied from source to destination
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
