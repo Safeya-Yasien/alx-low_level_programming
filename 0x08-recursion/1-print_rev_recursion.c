c510d7479d39c377948c477978f8d0119b037246
Safeya-Yasien <bluesky7yas@gmail.com>
2023-03-08 07:48:20 +0200

    reverse string

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _puts_recursion - function prints a string in reverse
+ * @s: string
+ * Return: nothing
+ */
+void _puts_recursion(char *s)
+{
+if (*s)
+{
+_puts_recursion(s+1);
+putchar(*s);
+}
+}
