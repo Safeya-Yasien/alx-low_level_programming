1cf25d4d2f9de5aaa87973bf1836319820dfc8f4
Safeya-Yasien <bluesky7yas@gmail.com>
2023-03-10 18:58:20 +0200

    number of arguments

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - print number of arguments
+ * @argc: number of arguments
+ * @argv: array of arguments
+ * Return: zero on success
+ */
+int main(int argc, char *argv[])
+{
+printf("%d\n", argc);
+return (0);
+}
+
