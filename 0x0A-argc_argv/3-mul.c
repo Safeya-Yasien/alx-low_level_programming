cb104280dad63d053a47c7789801cc08692ec17e
Safeya-Yasien <bluesky7yas@gmail.com>
2023-03-10 19:26:48 +0200

    multiplication of number

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - print multipulaction of number
+ * @argc: number of arguments
+ * @argv: array of arguments
+ * Return: zero on success
+ */
+int main(int argc, char *argv[])
+{
+if (argc < 3)
+{
+printf("error\n");   
+}
+else
+{
+printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+}
+return (0);
+}
