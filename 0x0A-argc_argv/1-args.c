e5933e5e74db4f43fb7b45f408dea4811dc45a4e
Safeya-Yasien <bluesky7yas@gmail.com>
2023-03-10 19:13:18 +0200

    number of arguments

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -7,7 +7,18 @@
  */
 int main(int argc, char *argv[])
 {
+int i;
+if (argc == 1)
+{
 printf("%d\n", argc - 1);
+}
+else
+{
+for (i = 0; *argv; i++; argv++)
+{
+printf("%d", i - 1);
+}
+}
 return (0);
 }
 
