cd101c605a9e5317ad09a36dd61669116ed18c20
Safeya-Yasien <bluesky7yas@gmail.com>
2023-03-10 19:46:27 +0200

    multiplication of number

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -8,18 +8,14 @@
  */
 int main(int argc, char *argv[])
 {
-int i, sum = 1;
-if (argc == 1 || argc == 2)
+if (argc < 3)
 {
-printf("error\n");   
+printf("Error\n");
+return (1);
 }
 else
 {
-for (i = 1; i < 3; i++)
-{
-sum *= atoi(argv[i]);
-}
-printf("%d\n", sum);
+printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
 }
 return (0);
 }
