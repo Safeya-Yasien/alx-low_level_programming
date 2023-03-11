d24b4ddc9a3ca4fce2684e14a6e1b58f1c6fbecf
Safeya-Yasien <bluesky7yas@gmail.com>
2023-03-11 03:38:16 +0200

    isdigit

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,30 +1,38 @@
 #include <stdio.h>
-#include <ctype.h>
 #include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
 #include "main.h"
 /**
- * main - Entry Point
- * @argc: arguments
- * @argv: array pointing to arguments
- * Return: 0
+ * main - Program that takes in all integer arguments and returns the sum
+ * @argc: Number of command line arguments
+ * @argv: Array name
+ * Return: 1 if a non-integer is among the passed in arguments, 0 otherwise
  */
 int main(int argc, char *argv[])
 {
-int i, sum = 0;
-if (argc < 1)
-{
+int i, j, length, sum;
+char *ptr;
+if (argc < 2)
 printf("0\n");
-return (0);
-}
+else
+{
+sum = 0;
 for (i = 1; i < argc; i++)
 {
-if (!atoi(argv[i]))
+ptr = argv[i];
+length = strlen(ptr);
+for (j = 0; j < length; j++)
 {
-printf("%s\n", "Error");
+if (isdigit(*(ptr + j)) == 0)
+{
+printf("Error\n");
 return (1);
+}
 }
 sum += atoi(argv[i]);
 }
 printf("%d\n", sum);
+}
 return (0);
 }
