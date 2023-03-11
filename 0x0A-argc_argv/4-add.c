7fd3caff0b771ea39e1881cfc0b9366b90161da3
Safeya-Yasien <bluesky7yas@gmail.com>
2023-03-11 03:11:15 +0200

    isdigit

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -12,26 +12,25 @@
 int main(int argc, char *argv[])
 {
 int sum = 0, i;
-if (argc == 1)
+if (argc < 1)
 {
 printf("0\n");
-return (0);
 }
 else
 {
 for (i = 1; i < argc; i++)
 {
-int j = atoi(argv[i]);
-if (isdigit(j) == 0)
-{
-sum += atoi(argv[i]);
-}
-else
+if (!(atoi(argv[i])))
 {
 printf("Error\n");
 return (1);
 }
+else
+{
+sum += atoi(argv[i]);
+}
 }
 printf("%d\n", sum);
 }
+return (0);
 }
