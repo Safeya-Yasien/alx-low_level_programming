1e65f1927e09050964befe56edd5987b7e2d973e
Safeya-Yasien <bluesky7yas@gmail.com>
2023-03-11 03:21:02 +0200

    isdigit

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,35 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <ctype.h>
+//#include "main.h"
 /**
- * main - add positive numbers
- * @argc: the number of arguments
- * @argv: array
- * Return: sum on success
- * 0 on if no number is passed
- * 1 if there is an error
+ * main - Entry Point
+ * @argc: arguments
+ * @argv: array pointing to arguments
+ * Return: 0
  */
 int main(int argc, char *argv[])
 {
-int sum = 0, i;
+int i, sum = 0;
 if (argc < 1)
 {
-printf("0\n");
+return (0);
 }
-else
-{
 for (i = 1; i < argc; i++)
 {
-if (!(atoi(argv[i])))
+if (!atoi(argv[i]))
 {
-printf("Error\n");
+printf("%s\n", "Error");
 return (1);
 }
-else
-{
 sum += atoi(argv[i]);
-}
-}
 }
 printf("%d\n", sum);
 return (0);
