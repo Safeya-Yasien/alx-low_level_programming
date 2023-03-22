53a42c9e775d50575287f5eeb8bf3c13b6c7f9c9
Safeya-Yasien <bluesky7yas@gmail.com>
2023-03-22 10:40:25 -0700

    print element in array by calling another function

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -9,9 +9,18 @@
  * @action: function which print elements
  * Return: void
  */
+
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	int i;
+	if (array == NULL)
+	{
+		return;
+	}
+	if (action == NULL)
+	{
+		return;
+	}
 	for (i = 0; i < size; i++)
 	{
 		action(array[i]);
