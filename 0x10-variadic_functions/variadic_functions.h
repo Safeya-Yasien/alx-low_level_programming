73297486c72a9bc39348e0aa87172995f90eb2a8
Safeya-Yasien <bluesky7yas@gmail.com>
2023-03-23 05:36:18 -0700

    variadic_functions

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,8 +1,6 @@
-#ifndef variadic_functions.h
 #ifndef VARIADIC_FUNCTIONS_H
-#define VARIADIC_FUNCTIONS_h
+#define VARIADIC_FUNCTIONS_H
 
-int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
