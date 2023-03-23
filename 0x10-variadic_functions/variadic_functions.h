681c69ca8be750fcc9216be9272096f9087807bb
Safeya-Yasien <bluesky7yas@gmail.com>
2023-03-23 05:41:22 -0700

    variadic_functions

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -6,4 +6,15 @@ void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
 
+/**
+ * struct print - multiple choice print
+ * @x: char Type of print
+ * @T_func: funct
+ */
+typedef struct print
+{
+	char *x;
+	void (*T_func)(va_list);
+} t_print;
+
 #endif
