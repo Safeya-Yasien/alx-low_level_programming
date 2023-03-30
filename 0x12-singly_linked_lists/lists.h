87a2b3ec4f7564b59a9a85839d38f8f62cb57350
Safeya-Yasien <bluesky7yas@gmail.com>
2023-03-30 16:25:27 +0200

    prototypes

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,6 +1,13 @@
 #ifndef LISTS.H
 #define LISTS.H
 
+typedef struct list_s
+{
+	char *str;
+	unsigned int len;
+	struct list_s *next;
+}
+
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
