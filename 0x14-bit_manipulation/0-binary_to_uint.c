f67bbcfdfb4b010b397fe1ad0f2bc2b72a16dda4
Safeya-Yasien <bluesky7yas@gmail.com>
2023-04-08 01:39:44 +0200

    convert binary number to decimal using bitwise operators

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -7,19 +7,30 @@
  * Return: If b is NULL or contains chars not 0 or 1 - 0.
  *         Otherwise - the converted number.
  */
+
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int num = 0;
-	int len = 0;
+	signed int i = 0, num = 0;
 
-	if (b[len] == '\0')
-		return (0);
-
-	while ((b[len] == '0') || (b[len] == '1'))
+	if (!b)
 	{
+		return (0);
+	}
+
+	while (b[i] != '\0')
+	{
+		if (b[i] != '0' && b[i] != '1')
+		{
+			return (0);
+		}
+
 		num <<= 1;
-		num += b[len] - '0';
-		len++;
+
+		if (b[i] & 1)
+		{
+			num += 1;
+		}
+		i++;
 	}
 
 	return (num);
