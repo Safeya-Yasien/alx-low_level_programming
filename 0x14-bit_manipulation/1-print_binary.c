8ab8f16efd0b00f901d883676742f7e5713e7d90
Safeya-Yasien <bluesky7yas@gmail.com>
2023-04-08 03:44:28 +0200

    convert decimal to binary

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,15 +1,23 @@
 #include "main.h"
 
-/**
- * print_binary - Prints the binary representation of a number.
- * @n: The number to be printed in binary.
+/*
+ * print_binary - print binary representation of a number
+ * @n: the decimal number
+ * Return: binary nubmer
  */
+
 void print_binary(unsigned long int n)
 {
 	if (n > 1)
 	{
 		print_binary(n >> 1);
 	}
-
-	_putchar((n & 1) + '0');
+	if (n & 1 )
+	{
+		_putchar('1');
+	}
+	else
+	{
+		_putchar('0');
+	}
 }
