3e1d893477df09cb95f39cac431b271eca407c46
Safeya-Yasien <bluesky7yas@gmail.com>
2023-04-11 03:38:07 +0200

    single linked list

--- 0x12-singly_linked_lists/list.h
+++ 0x12-singly_linked_lists/list.h
@@ -1,5 +1,6 @@
-#ifdef MAIN_H
-#define MAIN_H
+#ifndef LISTS_H
+#define LISTS_H
+#include <stddef.h>
 
 /**
  * struct list_s - singly linked list
@@ -9,6 +10,7 @@
  *
  * Description: singly linked list node structure
  */
+
 typedef struct list_s
 {
     char *str;
@@ -23,4 +25,4 @@ list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
 
-#endif	//MAIN_h
+#endif	//LIST_H
