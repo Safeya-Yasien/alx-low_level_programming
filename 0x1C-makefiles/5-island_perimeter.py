ca6d7d9e5da24aaad445feeeb9550c1f276400a8
Safeya-Yasien <bluesky7yas@gmail.com>
2023-10-25 22:43:23 +0300

    make files

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,13 +1,36 @@
 #!/usr/bin/python3
+"""
+Module Island Perimeter
+"""
+
+
 def island_perimeter(grid):
-    '''Is the function to find the island perimeter'''
-    count = 0
-    for i in range(len(grid)):
-        for j in range(len(grid[i])):
-            if grid[i][j] is 1:
-                count += 4
-                if j > 0 and grid[i][j - 1]:
-                    count -= 2
-                if i > 0 and grid[i - 1][j]:
-                    count -= 2
-    return count
+    """ Calculate perimeter of grid where "1" is found"""
+    p = 0
+    for row in range(len(grid)):
+        for col in range(len(grid[0])):
+            if grid[row][col] == 1:
+                if row == 0 or grid[row - 1][col] == 0:
+                    p += 1  # top
+                if row == (len(grid) - 1) or grid[row + 1][col] == 0:
+                    p += 1  # bottom
+                if col == 0 or grid[row][col - 1] == 0:
+                    p += 1  # left
+                if col == (len(grid[0]) - 1) or grid[row][col + 1] == 0:
+                    p += 1  # right
+    return p
+
+
+# Method for rectangular island only, no odd shapes
+#    maxWidth = 0
+#    length = 0
+#    for i in range(len(grid)):
+#        width = 0
+#        for j in range(len(grid[0])):
+#            if grid[i][j] == 1:
+#                width += 1
+#        if width:
+#            length += 1
+#        if width > maxWidth:
+#            maxWidth = width
+#    return ((maxWidth + length) * 2)
